e7e68c65307d49004db24ff1ad1dfcb188bae5c6
hemalatha563 <120002405+hemalatha563@users.noreply.github.com>
2023-04-25 20:54:14 +0530

    BackEND

---
+++ FullStackproject/FullStackprojectApplication.java
@@ -0,0 +1,20 @@
+package com.example.FullStackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+/* @SpringBootApplication- it has 3 annotations:
+ * 1.@ComponentScan- This annotation is used to specify the base packages to scan for spring beans/components.
+ * 2.@EnableAutoConfiguration- enables the magical auto configuration which can automatically configure.
+ * 3.@SpringBootConfiguration- This annotation indicates that a configuration class declares one or more @Bean methods.
+ * */
+
+@SpringBootApplication
+public class FullStackprojectApplication {
+       public static void main(String[] args) {
+		SpringApplication.run(FullStackprojectApplication.class, args);
+	}
+}
+
+
+
